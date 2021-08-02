ea9b1703e3ffef4fcd71b723385b7d344a463688
Johann Cruyff <isti.daroczi@gmail.com>
2021-08-02 23:04:09 +0300

    added exp description

--- src/components/EducationExp.jsx
+++ src/components/EducationExp.jsx
@@ -14,6 +14,8 @@ export class EducationExp extends Component {
       visibility1: false,
       visibility2: false,
       visibility3: false,
+      visibility4: false,
+      visibility5: false,
     };
   }
 
@@ -39,6 +41,18 @@ export class EducationExp extends Component {
   toggleVisibility3 = () => {
     this.setState({
       visibility3: !this.state.visibility3,
+    });
+  };
+
+  toggleVisibility4 = () => {
+    this.setState({
+      visibility4: !this.state.visibility4,
+    });
+  };
+
+  toggleVisibility5 = () => {
+    this.setState({
+      visibility5: !this.state.visibility5,
     });
   };
 
@@ -129,12 +143,90 @@ export class EducationExp extends Component {
         <div className="container2" data-aos="fade-left">
           <h2>Experience</h2>
           <h4>2019 Oct-2020 Nov</h4>
-          <h5>Topograph/Geodetic engineer - Global Topo SRL</h5>
+          <h5 className="job-experience">
+            Topograph/Geodetic engineer - Global Topo SRL
+            {this.state.visibility4 ? (
+              <FontAwesomeIcon
+                onClick={this.toggleVisibility4}
+                className="fa-1x dropup"
+                icon={faChevronUp}
+              />
+            ) : (
+              <FontAwesomeIcon
+                onClick={this.toggleVisibility4}
+                className="fa-1x dropdown"
+                icon={faChevronDown}
+              />
+            )}
+          </h5>
+          {this.state.visibility4 ? (
+            <div>
+              <h6>
+                <p>
+                  Followed local, regional and national guidelines for land
+                  surveying
+                </p>
+                <p>Performed field calculations to complete assessments</p>{" "}
+                <p>
+                  Measured and marked property lines and key topographic
+                  features
+                </p>{" "}
+                <p>
+                  Completed complex technical surveys for development of
+                  roadway, intersection and bridge construction plans
+                </p>{" "}
+                <p>
+                  Read and used topographical data to complete analysis of land
+                  plots and roadways
+                </p>{" "}
+                <p>
+                  Set up precise measurement and control equipment, including
+                  GPS and Total-stations
+                </p>
+              </h6>
+            </div>
+          ) : (
+            ""
+          )}
+
           <h4>2021 Jan-Jun</h4>
           <h5>
-            Some mini-projects at the Front-end Web-dev course in HTML, CSS, JS
-            and ReactJS
+            Front-End Web-Dev course experience
+            {this.state.visibility5 ? (
+              <FontAwesomeIcon
+                onClick={this.toggleVisibility5}
+                className="fa-1x dropup"
+                icon={faChevronUp}
+              />
+            ) : (
+              <FontAwesomeIcon
+                onClick={this.toggleVisibility5}
+                className="fa-1x dropdown"
+                icon={faChevronDown}
+              />
+            )}
           </h5>
+          
+            {this.state.visibility5 ? (
+              <div>
+                <h6>
+                  I have attended 5 month course at the informal School of IT
+                  and I’ve learned Web development concepts such as HTML5, CSS3,
+                  JavaScript (ECMAScript6 OOP concepts,variables, primitive
+                  types, AJAX, API, functions, hoisting, DOM ) and ReactJS. I’ve
+                  acquired knowledge in how to build a responsive, functional
+                  website. We had weekly projects which where meant to improve
+                  our knowledge and practice. We also learned about Github basic
+                  principles and commands. We had a final project, where we had
+                  to make our single-page-app online CV. The project can be
+                  found on my GitHub page and also there is a link in the
+                  contact section for it.
+                </h6>
+              </div>
+            ) : (
+              ""
+            )}
+          
         </div>
       </div>
     );
